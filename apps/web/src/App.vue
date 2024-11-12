863ea6d13d872332fa67b782932d897aba365821
see-more <964489530@qq.com>
2024-11-12 15:45:06 +0800

    init commit

---
+++ apps/web/src/App.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="underline text-red-500">Hello World</div>
+</template>
---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{md,js,ts,json,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
