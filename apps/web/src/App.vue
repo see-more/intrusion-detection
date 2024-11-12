17c77056fb20645cb36f4e988147021f10a9e710
see-more <964489530@qq.com>
2024-11-12 15:53:15 +0800

    chore:add prettier config file

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,6 +1,4 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 <template>
-  <div class="underline text-red-500">Hello World</div>
+    <div class="underline text-red-500">Hello World</div>
 </template>
